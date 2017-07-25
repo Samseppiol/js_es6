4132a640e5306c218f45896cfc7853ffe5b35fbb
Samseppiol <j_hage@live.com>
2017-07-25 16:45:29 +1000

    for each loops and a comparison to the legacy for loop

---
+++ readme.md
@@ -0,0 +1,1 @@
+Trying to gain a better understanding of javascript ES6 syntax.
---
+++ section_2/app.js
@@ -0,0 +1,8 @@
+let colours = ['red', 'black', 'green', 'red', 'black', 'green'];
+
+// For each array helper, another way instead of using buggy for loop.
+// Far less code than the standard for loop
+colours.forEach(function(colour) {
+  console.log(colour)
+})
+
