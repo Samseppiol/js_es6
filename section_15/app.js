6f347c7faf855fbe3c7f2a014e2517b1e8c1809e
Samseppiol <j_hage@live.com>
2017-08-13 18:45:26 +1000

    class inheritance in es6

--- section_15/app.js
+++ section_15/app.js
@@ -48,3 +48,23 @@ class Vehicle {
 const vehicle = new Vehicle({ title: 'Toyota'});
 console.log(vehicle)
 console.log(vehicle.drive)
+// Class inheritance.
+// However when we inherit from the Car class we want all of 
+// the cars methods, including the constructor which is looking
+// for a name
+class Subaru extends Car{
+  // Don't destructure when using class inheritance
+  // Simply pull out the whole object with options
+  constructor(options) {
+    super(options) //car constructor
+    this.color = options.color
+  }
+
+  honk() {
+    return 'beep'
+  }
+}
+
+const subaru = new Subaru({color: 'red'});
+console.log(subaru)
+console.log(subaru.honk)
--- section_15/readme.md
+++ section_15/readme.md
@@ -1,1 +1,3 @@
 ## Classes
+
+> When defining several functions/methods in a class you don't have to use a comma like you would with objects
